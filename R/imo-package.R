ca82ae437b22bcdad6922ee72f1bc8de038d0ca1
Patrick Sturm <sturm@tofwerk.com>
2024-11-11 11:45:00 +0100

    fix: correct example

--- R/imo-package.R
+++ R/imo-package.R
@@ -1,11 +1,6 @@
-#' Ion Mirror Optimization.
-#'
-#' The R package provides functions to optimize the geometry and voltages of 
-#' planar or axially symmetric ion mirrors, where the potential can be 
-#' calculated analytically. Design of expriments (DoE) and response surface 
-#' methodology (RSM) is used for efficient optimization.
-#'
-#' @docType package
-#' @author Patrick Sturm <sturm@tofwerk.com>
-#' @name imo
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
